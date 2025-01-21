32ede980ede3f052b29716c34c62adbbb1fe5e3e
Shikicaaa <stefanovicandrijasd@gmail.com>
2025-01-21 14:27:02 +0100

    Added test textBox

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 all:
-	gcc -I src/include -L src/lib -o main WinMain.cpp -lmingw32 -lSDL2main -lSDL2 -lm -lstdc++
+	gcc -I src/include -L src/lib -o main WinMain.cpp -lmingw32 -lSDL2main -lSDL2 -lSDL2_ttf -lm -lstdc++
 	
--- WinMain.cpp
+++ WinMain.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include <vector>
+#include <string>
 #include <math.h>
 
 const int width = 800;
@@ -9,15 +12,32 @@ const Uint32 blackColor = 0x00000000;
 const double gForce = 9.81;
 const double maxSpeed = 20;
 const double elasticity = 0.8;
-const double friction = 1;
+const double friction = 0.99;
+const Uint32 textColor = 0xFFD3D3D3;
+const SDL_Color text = {0,0,0,255};
 
-class Circle{
+class Circle : SDL_Rect{
     public:
         double x;
         double y;
         double radius;
         Circle(double xCord, double yCord, double rad) : x(xCord), y(yCord), radius(rad){}
         ~Circle(){}
+};
+
+class Container{
+    public:
+        double x,y;
+        double height, width;
+        std::vector<SDL_Rect> objects;
+        Container(double xPosition, double yPosition, double h, double w) : x(xPosition),y(yPosition),height(h),width(w){}
+        inline void addObjects(SDL_Rect object) {objects.push_back(object);}
+        bool removeObject(SDL_Rect* object){
+            bool found = false;
+            return found;
+        }
+
+
 };
 
 void FillCircle(SDL_Surface* surface, Circle circle, Uint32 color){
@@ -34,6 +54,24 @@ void FillCircle(SDL_Surface* surface, Circle circle, Uint32 color){
         }
     }
 }
+
+bool CheckIfInTxt(SDL_Event event, SDL_Rect textBox){
+    double mouseX = event.motion.x;
+    double mouseY = event.motion.y;
+    std::cout << "Checking..." << textBox.x << " " << textBox.y << " " << event.motion.x << " " << event.motion.y <<std::endl;
+    return (mouseX >= textBox.x && mouseX <= textBox.w + textBox.x) && (mouseY >= textBox.y && mouseY <= textBox.h + textBox.y);
+}
+
+void FocusTextBox(int txtBoxNum, std::vector<bool>* textBoxes){
+    if(txtBoxNum < -1) return;
+    if(txtBoxNum != -1)
+        for(int i = 0;i<textBoxes->size();i++)
+            textBoxes->at(i) = i == txtBoxNum ? true : false;
+    else
+        for(int i = 0;i<textBoxes->size();i++)
+            textBoxes->at(i) = false;
+}
+
 void CalculatCoordinates(SDL_Event event, Circle* circle, SDL_Surface* surface){
     if (event.motion.x + circle->radius >= surface->w) {
         circle->x = surface->w - circle->radius;
@@ -53,16 +91,6 @@ void CalculatCoordinates(SDL_Event event, Circle* circle, SDL_Surface* surface){
 }
 
 int main(int argc, char *argv[]){
-    SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Window* window = SDL_CreateWindow("Bouncing Ball",SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,width,height,0);
-    
-    SDL_Surface* surface = SDL_GetWindowSurface(window);
-
-    Circle krug = Circle(200,200,80);
-
-    bool exitCondition = true;
-    SDL_Event event;
-    SDL_Rect clearScreen = {0,0,width,height};
     double acceleration = gForce;
     double initialVelocity;
     double speedY = 0;
@@ -74,7 +102,40 @@ int main(int argc, char *argv[]){
     double firstPositionY;
     double lastPositionY;
     double lastPositionX;
+
+    //INITS
+    SDL_Init(SDL_INIT_EVERYTHING);
+    TTF_Init();
+    SDL_StartTextInput();
+
+    SDL_Window* window = SDL_CreateWindow("Bouncing Ball",SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,width,height,0);
+
+    TTF_Font* font = TTF_OpenFont("assets/Arial.ttf",16);
+    if(!font){
+        std::cout << "Greska pri otvaranju fonta! " << SDL_GetError() << std::endl;
+        return -1;
+    }
+
+    SDL_Surface* surface = SDL_GetWindowSurface(window);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window,-1,SDL_RENDERER_ACCELERATED);
+
+    Circle krug = Circle(200,200,80);
+
+    bool exitCondition = true;
+    std::vector<bool> textBoxes(7,0);
+    std::vector<std::string> textBoxesText(7,"");
+    std::vector<SDL_Rect> textBoxList(7,{640,10,160,30});
+
+
+    SDL_Event event;
+
+    SDL_Rect clearScreen = {0,0,width,height};
+    SDL_Rect txtBox0 = {640,10,160,30};
+
     while(exitCondition){
+        
+        // EVENT HANDLING
+
         while(SDL_PollEvent(&event)){
             if(event.type == SDL_QUIT){
                 exitCondition = false;
@@ -88,8 +149,13 @@ int main(int argc, char *argv[]){
             if(event.type == SDL_MOUSEBUTTONDOWN){
                 firstPositionX = event.motion.x;
                 firstPositionY = event.motion.y;
-                std::cout<<"First Position of X: " << firstPositionX << std::endl;
-                std::cout<<"First Position of Y: " << firstPositionY << std::endl;
+
+                for(int i = 0;i<textBoxes.size();i++){
+                    if(CheckIfInTxt(event,textBoxList[i])){
+                        FocusTextBox(i,&textBoxes);
+                        break;
+                    }
+                }
             }
             if(event.type == SDL_MOUSEBUTTONUP){
                 lastPositionX = event.motion.x;
@@ -99,7 +165,28 @@ int main(int argc, char *argv[]){
                 falling = true;
                 sliding = true;
                 moving = false;
-                std::cout<<"Second Position: " << lastPositionX << std::endl;
+            }
+            if (event.type == SDL_KEYDOWN) {
+                for (int i = 0; i < textBoxes.size(); i++) {
+                    if (textBoxes[i]) {
+                        if (event.key.keysym.sym == SDLK_BACKSPACE) {
+                            if (!textBoxesText[i].empty()) {
+                                textBoxesText[i].pop_back();
+                                std::cout << "Obrisao: " << textBoxesText[i] << std::endl;
+                            }
+                        }
+                        if(event.key.keysym.sym == SDLK_RETURN){
+                            std::cout << "TextBox" << i << ":" << textBoxesText[i] << std::endl;
+                        }
+                    }
+                }
+            }
+            if (event.type == SDL_TEXTINPUT) {
+                for (int i = 0; i < textBoxes.size(); i++) {
+                    textBoxesText[i] += event.text.text;
+                    std::cout << "Pisem: " << event.text.text << std::endl;
+                    break;
+                }
             }
         }
         if(falling && !moving){
@@ -143,8 +230,20 @@ int main(int argc, char *argv[]){
                 if(krug.x-krug.radius <= 2) krug.x = 0;
             }
         }
+        //RENDERING
         SDL_FillRect(surface,&clearScreen,blackColor);
         FillCircle(surface,krug,whiteColor);
+        SDL_FillRect(surface,&textBoxList[0],textBoxes[0] ? whiteColor : textColor);
+        for(int i = 0;i<textBoxes.size();i++){
+            if(!textBoxesText[i].empty()){
+                SDL_Surface* textSurface = TTF_RenderText_Blended(font,textBoxesText[i].c_str(),{0,0,0,255});
+                SDL_Rect textPosition = textBoxList[i];  
+                textPosition.x += 5;
+                textPosition.y += (textBoxList[i].h - textSurface->h) / 2;
+                SDL_BlitSurface(textSurface,NULL,surface,&textPosition);
+                SDL_FreeSurface(textSurface);
+            }
+        }
         SDL_UpdateWindowSurface(window);
         SDL_Delay(20);
     }
